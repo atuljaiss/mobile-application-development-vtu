f3ab90c0f69f6db79bec590ddfb683c535f45560
Atul Kumar <68207869+atuljaiss@users.noreply.github.com>
2022-07-18 00:16:12 +0530

    7&8 update
    Added text to speech and call activity

---
+++ 7. Text-To-Speech-Android-working/app/src/main/java/com/example/texttospeech/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.texttospeech;
+
+import android.os.Build;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSpeak;
+    EditText editText;
+    TextToSpeech textToSpeech;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Init TextToSpeech
+        textToSpeech = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                       btnSpeak.setEnabled(true);
+                        speak();
+                }
+            }
+        });
+
+        // Init View
+        btnSpeak = (Button)findViewById(R.id.btnSpeak);
+        editText = (EditText)findViewById(R.id.editText);
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak();
+            }
+        });
+    }
+
+    private void speak() {
+        String text = editText.getText().toString();
+        textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH,null,null);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        } else {
+            textToSpeech.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+}
